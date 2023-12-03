002f22e41d97c4f46e2b2d35788e256ca4919d45
Nathan Richards <nld@logios.co.uk>
2023-12-03 10:42:22 +0000

    Refactored day 3

--- day03/index.js
+++ day03/index.js
@@ -18,43 +18,46 @@ function parse(lines) {
     const symbols = [];
 
     for (let y=0; y<lines.length; y++) {
-        let num = undefined;
+        let number = undefined;
+        const pushNumber = () => {
+            number.value = Number(number.value);
+            numbers.push(number);
+            log(number);
+            number = undefined;
+        };
+
         for (let x=0; x<lines[y].length; x++) {
             const c = lines[y][x];
-            if (isNaN(Number(c)) && num) {
-                num.value = Number(num.value);
-                    numbers.push(num);
-                    log(num);
-                    num = undefined;
+            if (isNaN(Number(c)) && number) {
+                pushNumber();
             }
 
             if (c === '.') {
                 continue;
             }
             if (!isNaN(Number(c))) {
-                if (num) {
-                    num.value += c;
-                    num.end[0]++;
+                if (number) {
+                    number.value += c;
+                    number.end[0]++;
                 } else {
-                    num = {
+                    number = {
                         value: c,
                         start: [x, y],
                         end: [x, y]
                     }
                 }
             } else {
-                const s = {
+                const symbol = {
                     value: c,
                     position: [x, y]
                 };
-                log(s);
-                symbols.push(s);
+                log(symbol);
+                symbols.push(symbol);
             }
         }
-        if (num) {
-            num.value = Number(num.value);
-            numbers.push(num);
-            log(num);
+
+        if (number) {
+            pushNumber();
         }
     }
     return {
@@ -63,55 +66,35 @@ function parse(lines) {
     };
 }
 
-function part1(numbers, symbols) {
-    const parts = numbers.filter(num => {
-        log(`Checking number ${num.value}, (${num.start} -> ${num.end})`);
-        const adjacentSymbol = symbols.find(s => {
-            const ny = num.start[1];
-            const nxS = num.start[0];
-            const nxE = num.end[0];
-            const [sx, sy] = s.position;
-            if (Math.abs(sy - ny) <= 1) {
-                return sx <= nxE + 1 && sx >= nxS -1;
-            } else {
-                return false;
-            }
-        });
-
-        if (adjacentSymbol) {
-            log(`Found adjacent symbol ${adjacentSymbol.value} at [${adjacentSymbol.position}]`);
-            return true;
-        } else {
-            log(`No adjacent symbol fouund`);
-            return false;
-        }
-    });
+function isAdjacent(number, symbol) {
+    // Define a bounding box around the number
+    const topLeft = [number.start[0]-1, number.start[1]-1];
+    const bottomRight = [number.end[0]+1, number.end[1]+1];
     
-    return parts.map(n => n.value).reduce((p,c) => p+c);
+    // Check symbol coordinates inside box
+    const [x, y] = symbol.position;
+    return x >= topLeft[0] && x <= bottomRight[0] && y >= topLeft[1] && y <= bottomRight[1];
+}
+
+function part1(numbers, symbols) {
+    return numbers
+        .filter(n => symbols.find(s => isAdjacent(n, s)))
+        .map(n => n.value)
+        .reduce((p,c) => p+c);
 }
 
 function part2(numbers, symbols) {
     return symbols
         .filter(s => s.value === '*')
         .map(s => {
-            const adjacentNumbers = numbers.filter(num => {
-                const ny = num.start[1];
-                const nxS = num.start[0];
-                const nxE = num.end[0];
-                const [sx, sy] = s.position;
-                if (Math.abs(sy - ny) <= 1) {
-                    return sx <= nxE + 1 && sx >= nxS -1;
-                } else {
-                    return false;
-                }
-            })
+            const adjacentNumbers = numbers.filter(n => isAdjacent(n, s));
             if (adjacentNumbers.length === 2) {
                 return adjacentNumbers[0].value * adjacentNumbers[1].value;
             } else {
                 return 0;
             }
         })
-    .reduce((p, c) => p+c);
+        .reduce((p, c) => p+c);
 }
 
 const lines = raw.trim().split('\n').map(l => l.trim());
