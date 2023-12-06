33aad11e62322c70dba4572ddba211f37e7a2a9e
Nathan Richards <nld@logios.co.uk>
2023-12-06 11:48:43 +0000

    Refactored day 6

--- day06/index.js
+++ day06/index.js
@@ -5,35 +5,48 @@ if (DEBUG) {
     Distance:  9  40  200`;
 }
 
-function parse(lines) {
-    let [_, ...times] = lines[0].split(/\s+/);
-    let [__, ...distances] = lines[1].split(/\s+/);
-
-    return times.map((t, i) => [Number(t), Number(distances[i])]);
-}
-
-function solve(t, c) {
-    // x * (t - x) = c
-    // -x^2 + tx - c = 0
+/**
+ * Calculates the 2 solutions that will give the specified distance in the specified time.
+ */
+function solve(totalTime, recordDistance) {
+    // The distance travelled can be calculated as follows:
+    //    d = w * (t - w)
+    // Where d is the total distance travelled
+    //       w is the time spent holding the button
+    //       t is the total race time
+    //
+    // Rearranging this, we can say:
+    //     w * (t - w) = d
+    //   -w^2 + tw - d = 0
+    //
+    // This is a quadratic equation that we can solve with
+    // the standard quadratic formula.
 
     const a = -1;
-    const b = t;
-    c = -c;
+    const b = totalTime;
+    const c = -recordDistance;
 
-    const pos = (-b + Math.sqrt((b*b) - 4*a*c)) / (2*a);
-    const neg = (-b - Math.sqrt((b*b) - 4*a*c)) / (2*a);
+    const x1 = (-b + Math.sqrt((b*b) - 4*a*c)) / (2*a);
+    const x2 = (-b - Math.sqrt((b*b) - 4*a*c)) / (2*a);
 
-    log ([pos, neg]);
+    log ([x1, x2]);
 
-    return [Math.floor(pos), Math.ceil(neg)];
+    // We need discrete (integer) values for the problem.
+    // So we round to the solutions to the lowest/highest integers
+    // such that the total distance < record distance.
+    // Note - for our equation, x1 < x2
+    return [Math.floor(x1), Math.ceil(x2)];
 }
 
 function part1(lines) {
-    const races = parse(lines);
+    lines = lines.map(l => l.split(/\s+/).slice(1).map(Number));
+    const races = lines[0].map((t, i) => [t, lines[1][i]]);
     log(races);
+
     return races.map(([t, c]) => {
         const [min, max] = solve(t, c);
         log ([min, max]);
+
         return max - min - 1;
     }).reduce((p, c) => p*c);
 }
@@ -43,6 +56,7 @@ function part2(lines) {
     const distance = Number(lines[1].substring(10).replace(/\s+/g,''));
     log(time);
     log(distance);
+
     const [min, max] = solve(time, distance);
     return max - min - 1;
 }
