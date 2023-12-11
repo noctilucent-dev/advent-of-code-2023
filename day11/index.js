177e84aac6167e0f5af710cb00474b3c56a87814
Nathan Richards <nld@logios.co.uk>
2023-12-11 14:00:02 +0000

    Refactored day 11

--- day11/index.js
+++ day11/index.js
@@ -38,7 +38,7 @@ function print(galaxies) {
     return lines.join('\n');
 }
 
-function part1(lines) {
+function parse(lines) {
     let galaxies = [];
     for (let y=0; y<lines.length; y++) {
         for (let x=0; x<lines[y].length; x++) {
@@ -65,62 +65,15 @@ function part1(lines) {
     log(blankLines);
     log(blankColumns);
 
-    log(print(galaxies));
-
-    galaxies = galaxies.map(([x,y]) => {
-        const rightShift = blankColumns.filter(c => c < x).length;
-        const downShift = blankLines.filter(l => l < y).length;
-        return [
-            x+rightShift,
-            y+downShift
-        ];
-    });
-
-    log(print(galaxies));
-
-
-    return galaxies.reduce((p, g, i) => {
-        return p + galaxies.slice(i+1).map((o, j) => {
-            const distance = Math.abs(g[0] - o[0]) + Math.abs(g[1] - o[1]);
-            log(`Galaxy ${i+1} -> ${i+j+2} = ${distance}`);
-            return distance;
-        }).reduce((p, c) => p + c, 0);
-    }, 0);
-
+    return {
+        galaxies,
+        blankLines,
+        blankColumns
+    };
 }
 
-function part2(lines) {
-    let galaxies = [];
-    for (let y=0; y<lines.length; y++) {
-        for (let x=0; x<lines[y].length; x++) {
-            if (lines[y][x] === '#') {
-                galaxies.push([x,y]);
-            }
-        }
-    }
-
-    const blankLines = [];
-    const blankColumns = [];
-    for (let y=0; y<lines.length; y++) {
-        if (lines[y].indexOf('#') === -1) {
-            blankLines.push(y);
-        }
-    }
-    for (let x=0; x<lines[0].length; x++) {
-        if (lines.map(l => l[x]).indexOf('#') === -1) {
-            blankColumns.push(x);
-        }
-    }
-
-    log(galaxies);
-    log(blankLines);
-    log(blankColumns);
-
-    log(print(galaxies));
-
-    const expansion = 1000000;
-
-    galaxies = galaxies.map(([x,y]) => {
+function expand(galaxies, blankColumns, blankLines, expansion) {
+    return galaxies.map(([x,y]) => {
         const rightShift = blankColumns.filter(c => c < x).length;
         const downShift = blankLines.filter(l => l < y).length;
         return [
@@ -128,10 +81,9 @@ function part2(lines) {
             y+(downShift * (expansion-1))
         ];
     });
+}
 
-    log(print(galaxies));
-
-
+function sumShortestPaths(galaxies) {
     return galaxies.reduce((p, g, i) => {
         return p + galaxies.slice(i+1).map((o, j) => {
             const distance = Math.abs(g[0] - o[0]) + Math.abs(g[1] - o[1]);
@@ -139,7 +91,28 @@ function part2(lines) {
             return distance;
         }).reduce((p, c) => p + c, 0);
     }, 0);
+}
 
+function part1(lines) {
+    let { galaxies, blankColumns, blankLines } = parse(lines);
+    log(print(galaxies));
+
+    galaxies = expand(galaxies, blankColumns, blankLines, 2);
+    log(print(galaxies));
+
+    return sumShortestPaths(galaxies);
+}
+
+function part2(lines) {
+    let { galaxies, blankColumns, blankLines } = parse(lines);
+    log(print(galaxies));
+
+    const expansion = DEBUG ? 10 : 1000000;
+
+    galaxies = expand(galaxies, blankColumns, blankLines, expansion);
+    log(print(galaxies));
+
+    return sumShortestPaths(galaxies);
 }
 
 const lines = toTrimmedLines(raw);
